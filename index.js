fbd0c13ae55cb013b79e2064383baa9f87d55c2a
Alex Martin <alex.pmartin1@gmail.com>
2021-09-20 21:45:13 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Memphis';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;//When I used single quotes instead of backticks, interpolation did not work for me. Today I learned!
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
